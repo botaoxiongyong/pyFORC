8a8c364acb0dcb186b4e4a76d0531a6d46ff7fe7
Jiabo <kabol.liu@gmail.com>
2018-08-29 16:04:30 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+NAME = "pyFORC"
+
+setup(
+    name=NAME,
+    version='0.0.1',
+    author='Liu Jiabo',
+    author_email='kabol.liu@gmail.com',
+    packages=['pyFORC_log']
+)
